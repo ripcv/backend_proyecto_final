e69ebd351240ac36ccb569d14a8fd22347d0d004
Ripcv <gefallene.engel@gmail.com>
2024-09-12 20:17:12 -0300

    Se realiza modificaciones en la visual del cart

--- entrega_final/src/public/js/cart.js
+++ entrega_final/src/public/js/cart.js
@@ -1,44 +1,70 @@
 document.getElementById('purchaseForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const form = event.target;
-    const cartId = form.cartId.value;
+    const { value: formValues } = await Swal.fire({
+        title: 'Infomación de envio',
+        html: `
+            <input id="swal-input1" class="swal2-input" placeholder="Dirección de envío">
+            <input id="swal-input2" class="swal2-input" placeholder="Ciudad">
+        `,
+        focusConfirm: false,
+        preConfirm: () => {
+            const shippingAddress = document.getElementById('swal-input1').value;
+            const city = document.getElementById('swal-input2').value;
 
-    try {
-        const response = await fetch(form.action, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ cartId })
-        });
+            if (!shippingAddress || !city) {
+                Swal.showValidationMessage('Por favor, complete todos los campos');
+                return false;
+            }
 
-        if (!response.ok) {
-            throw new Error('Error al procesar la compra');
+            return [shippingAddress, city];
         }
+    })
 
-        const result = await response.json();
-        let mensaje = " "
-            if(!result.products){
-                mensaje = "El carrito esta vacio, favor agregar productos"
-            }else{
-                const products = result.products
+    if (formValues) {
+        const form = event.target;
+        const cartId = form.cartId.value;
+        const shippingAddress = formValues[0];  
+        const city = formValues[1];  
+
+        try {
+            const response = await fetch(form.action, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    cartId,
+                    shippingAddress,
+                    city
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al procesar la compra');
+            }
+
+            const result = await response.json();
+            let mensaje = " "
+            if (!result.products) {
+                mensaje = "El carrito está vacío, favor agregar productos";
+            } else {
+                const products = result.products;
                 mensaje = products.join('<br>') 
-                if(products.length>0){
-                    mensaje+= "<br> sin Stock suficiente."
+                if (products.length > 0) {
+                    mensaje += "<br> sin stock suficiente.";
                 }
             }
 
-        if (result.success) {
-            // Mostrar una alerta de éxito
-            sweetAlert("top-end","success",`Compra procesada exitosamente.<br>${mensaje}`,`/carts/${cartId}`)
-          
-        } else {
-            sweetAlert("top-end","error",`Error al procesar la compra.<br>${mensaje}`,`/carts/${cartId}`)
+            if (result.success) {
+                // Mostrar una alerta de éxito
+                sweetAlert("top-end","success",`Compra procesada exitosamente.<br>${mensaje}`,`/carts/${cartId}`);
+            } else {
+                sweetAlert("top-end","error",`Error al procesar la compra.<br>${mensaje}`,`/carts/${cartId}`);
+            }
+        } catch (error) {
+            console.error(error);
         }
-    } catch (error) {
-       // console.log(error)
-
     }
 });
 
